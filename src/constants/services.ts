1829a888cdc9e4a3269b706e7f7ca9454b752bbd
Wakar <waqar786@hotmail.de>
2025-09-09 19:02:00 +0200

    anpassen der preise

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -1,14 +1,14 @@
 export const parkingPrices = [
-  { days: 1, price: 48 }, { days: 2, price: 50 }, { days: 3, price: 53 },
-  { days: 4, price: 58 }, { days: 5, price: 61 }, { days: 6, price: 66 },
-  { days: 7, price: 75 }, { days: 8, price: 78 }, { days: 9, price: 80 },
-  { days: 10, price: 83 }, { days: 11, price: 86 }, { days: 12, price: 90 },
-  { days: 13, price: 94 }, { days: 14, price: 110 }, { days: 15, price: 114 },
-  { days: 16, price: 115 }, { days: 17, price: 119 }, { days: 18, price: 123 },
-  { days: 19, price: 126 }, { days: 20, price: 130 }, { days: 21, price: 134 },
-  { days: 22, price: 137 }, { days: 23, price: 140 }, { days: 24, price: 148 },
-  { days: 25, price: 151 }, { days: 26, price: 154 }, { days: 27, price: 160 },
-  { days: 28, price: 163 }, { days: 29, price: 170 }, { days: 30, price: 176 },
+  { days: 1, price: 40 }, { days: 2, price: 43 }, { days: 3, price: 45 },
+  { days: 4, price: 48 }, { days: 5, price: 50 }, { days: 6, price: 55 },
+  { days: 7, price: 59 }, { days: 8, price: 63 }, { days: 9, price: 65 },
+  { days: 10, price: 70 }, { days: 11, price: 72 }, { days: 12, price: 75 },
+  { days: 13, price: 78 }, { days: 14, price: 80 }, { days: 15, price: 85 },
+  { days: 16, price: 88 }, { days: 17, price: 90 }, { days: 18, price: 93 },
+  { days: 19, price: 95 }, { days: 20, price: 100 }, { days: 21, price: 104 },
+  { days: 22, price: 107 }, { days: 23, price: 110 }, { days: 24, price: 113 },
+  { days: 25, price: 115 }, { days: 26, price: 118 }, { days: 27, price: 120 },
+  { days: 28, price: 123 }, { days: 29, price: 125 }, { days: 30, price: 130 },
 ];
 
 export const services = [
